110c98b562af87a27cc676656523afb4d0001d26
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-05-04 20:55:00 +0600

    netalbum-api: Images, добавление библиотеки Thumbnailator

---
+++ netalbum-api/src/main/java/ru/danilakondr/netalbum/api/Images.java
@@ -0,0 +1,31 @@
+package ru.danilakondr.netalbum.api;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import net.coobird.thumbnailator.Thumbnails;
+
+public class Images {
+	public static ImageData generateImage(File f, String name, int thumbnailWidth, int thumbnailHeight) throws IOException {
+		ImageData data = new ImageData();
+		data.setFileName(name);
+		data.setFileSize(f.length());
+		
+		BufferedImage img = ImageIO.read(f);
+		data.setWidth(img.getWidth());
+		data.setHeight(img.getHeight());
+		
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		Thumbnails.of(img)
+			.size(thumbnailWidth, thumbnailHeight)
+			.outputFormat("JPEG")
+			.toOutputStream(os);
+		
+		data.setThumbnail(os.toByteArray());
+		return data;
+	}
+}
