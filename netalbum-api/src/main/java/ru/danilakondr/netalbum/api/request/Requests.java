06bf84fa5f8570ba454172bc5f20fc3455ee9e1f
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-05-04 21:17:20 +0600

    netalbum-api: Requests: запросы
    connectToSession
    disconnectFromSession
    closeSession

--- netalbum-api/src/main/java/ru/danilakondr/netalbum/api/request/Requests.java
+++ netalbum-api/src/main/java/ru/danilakondr/netalbum/api/request/Requests.java
@@ -1,6 +1,7 @@
 package ru.danilakondr.netalbum.api.request;
 
 import ru.danilakondr.netalbum.api.DirectoryName;
+import ru.danilakondr.netalbum.api.SessionId;
 
 public class Requests {
 	public static Request<DirectoryName> initSession(String directoryName) {
@@ -13,5 +14,25 @@ public class Requests {
 		request.setContents(contents);
 		
 		return request;
+	}
+	
+	public static Request<SessionId> connectToSession(String sessionId) {
+		Request<SessionId> request = new Request<>();
+		request.setMethod("connectToSession");
+		
+		SessionId contents = new SessionId();
+		contents.setSessionId(sessionId);
+		
+		request.setContents(contents);
+		
+		return request;
+	}
+	
+	public static Request<Void> disconnectFromSession() {
+		return new Request<Void>("disconnectFromSession");
+	}
+	
+	public static Request<Void> closeSession() {
+		return new Request<Void>("closeSession");
 	}
 }
