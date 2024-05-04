79887c34f0404f73aa034ca7d325e8f967c080a7
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-05-04 20:21:04 +0600

    netalbum-api: Request

---
+++ netalbum-api/src/main/java/ru/danilakondr/netalbum/api/request/Request.java
@@ -0,0 +1,35 @@
+package ru.danilakondr.netalbum.api.request;
+
+/**
+ * Объект запроса, посылаемого в формате JSON.
+ * <p>
+ * Формат запроса:
+ * <pre>
+ * {"method": строка-название метода,
+ *  "contents": объект с произвольным содержимым
+ * }
+ * </pre>
+ * @param <T> тип содержимого
+ */
+public class Request<T> {
+	private String method;
+	private T contents;
+
+	public Request() {}
+	
+	public String getMethod() {
+		return method;
+	}
+	
+	public void setMethod(String method) {
+		this.method = method;
+	}
+	
+	public T getContents() {
+		return contents;
+	}
+	
+	public void setContents(T contents) {
+		this.contents = contents;
+	}
+}
