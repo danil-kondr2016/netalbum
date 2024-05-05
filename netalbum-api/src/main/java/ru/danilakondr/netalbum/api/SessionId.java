09205f92be1d643165039627d8c9730e870100a8
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-05-05 14:20:19 +0600

    netalbum-api: SessionId

---
+++ netalbum-api/src/main/java/ru/danilakondr/netalbum/api/SessionId.java
@@ -0,0 +1,13 @@
+package ru.danilakondr.netalbum.api;
+
+public class SessionId {
+	private String sessionId;
+	
+	public String getSessionId() {
+		return sessionId;
+	}
+	
+	public void setSessionId(String id) {
+		this.sessionId = id;
+	}
+}
