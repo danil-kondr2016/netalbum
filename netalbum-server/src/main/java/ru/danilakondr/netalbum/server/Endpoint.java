70d3c099d1d5bc7d6865dd6f1c90a67eddc5c27d
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-05-06 18:05:18 +0600

    netalbum-server: Тестовый сервер

--- netalbum-server/src/main/java/ru/danilakondr/netalbum/server/Endpoint.java
+++ netalbum-server/src/main/java/ru/danilakondr/netalbum/server/Endpoint.java
@@ -3,14 +3,17 @@ package ru.danilakondr.netalbum.server;
 import java.io.IOException;
 import java.util.Map;
 
+import javax.websocket.EncodeException;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
+import javax.websocket.RemoteEndpoint;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 import ru.danilakondr.netalbum.api.request.Request;
+import ru.danilakondr.netalbum.api.response.Responses;
 
 @ServerEndpoint(
 		value="/",
@@ -24,16 +27,29 @@ public class Endpoint {
 	
 	@OnMessage
     public void onMessage(Session session, Request<?> req) throws IOException {
-        // Handle new messages
+		try {
+			session.getBasicRemote().sendObject(Responses.message("sorry, but server has not been implemented"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			session.getBasicRemote().sendText("thrown "+e);
+		} catch (EncodeException e) {
+			e.printStackTrace();
+			session.getBasicRemote().sendText("thrown "+e);
+		}
     }
 
     @OnClose
     public void onClose(Session session) throws IOException {
-        // WebSocket connection closes
+        
     }
 
     @OnError
-    public void onError(Session session, Throwable throwable) {
-        // Do error handling here
+    public void onError(Session session, Throwable throwable) throws IOException {
+    	try {
+			session.getBasicRemote().sendText("error "+throwable);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
