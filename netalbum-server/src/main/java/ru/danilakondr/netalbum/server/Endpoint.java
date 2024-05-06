ca637f15ccacecc5274fa5881abb63285aff44c3
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-05-06 18:28:55 +0600

    netalbum-server: Endpoint: добавление обработки запросов
    Обработка запросов пока реализована неполноценно, однако ответы
    приходят, и это хорошо.

--- netalbum-server/src/main/java/ru/danilakondr/netalbum/server/Endpoint.java
+++ netalbum-server/src/main/java/ru/danilakondr/netalbum/server/Endpoint.java
@@ -12,7 +12,9 @@ import javax.websocket.RemoteEndpoint;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import ru.danilakondr.netalbum.api.SessionId;
 import ru.danilakondr.netalbum.api.request.Request;
+import ru.danilakondr.netalbum.api.response.Response;
 import ru.danilakondr.netalbum.api.response.Responses;
 
 @ServerEndpoint(
@@ -20,21 +22,46 @@ import ru.danilakondr.netalbum.api.response.Responses;
 		decoders= {GenericRequestDecoder.class},
 		encoders= {GenericResponseEncoder.class})
 public class Endpoint {
+	private String sessionId;
+	
 	@OnOpen
 	public void onOpen(Session session) throws IOException {
 		
 	}
 	
 	@OnMessage
-    public void onMessage(Session session, Request<?> req) throws IOException {
-		try {
-			session.getBasicRemote().sendObject(Responses.message("sorry, but server has not been implemented"));
-		} catch (IOException e) {
-			e.printStackTrace();
-			session.getBasicRemote().sendText("thrown "+e);
-		} catch (EncodeException e) {
-			e.printStackTrace();
-			session.getBasicRemote().sendText("thrown "+e);
+    public void onMessage(Session session, Request<?> req) throws IOException, EncodeException {
+		switch (req.getMethod()) {
+		case INIT_SESSION:
+			String id = SessionIdProvider.generateSessionId();
+			Response<?> resp = Responses.sessionId(id);
+			session.getBasicRemote().sendObject(resp);
+			this.sessionId = id;
+			
+			break;
+		case CONNECT_TO_SESSION:
+			Request<SessionId> req_sessionId = (Request<SessionId>)req;
+			this.sessionId = req_sessionId.getContents().getSessionId();
+			break;
+		case DISCONNECT_FROM_SESSION:
+			session.getBasicRemote().sendObject(Responses.success());
+			session.close();
+			break;
+		case CLOSE_SESSION:
+			session.getBasicRemote().sendObject(Responses.success());
+			session.close();
+			break;
+		case ADD_IMAGES:
+			session.getBasicRemote().sendObject(Responses.success());
+			break;
+		case DOWNLOAD_CONTENTS:
+			break;
+		case GET_DIRECTORY_INFO:
+			break;
+		case SYNCHRONIZE:
+			break;
+		default:
+			break;
 		}
     }
 
