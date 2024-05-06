a40e12c52321c8c00c9e4bd3758fc827ceec888f
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-05-06 20:13:58 +0600

    netalbum-server: расширение обработки ошибок

--- netalbum-server/src/main/java/ru/danilakondr/netalbum/server/Endpoint.java
+++ netalbum-server/src/main/java/ru/danilakondr/netalbum/server/Endpoint.java
@@ -12,8 +12,10 @@ import javax.websocket.RemoteEndpoint;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import jakarta.json.bind.JsonbException;
 import ru.danilakondr.netalbum.api.SessionId;
 import ru.danilakondr.netalbum.api.request.Request;
+import ru.danilakondr.netalbum.api.request.RequestType;
 import ru.danilakondr.netalbum.api.response.Response;
 import ru.danilakondr.netalbum.api.response.Responses;
 
@@ -22,7 +24,7 @@ import ru.danilakondr.netalbum.api.response.Responses;
 		decoders= {GenericRequestDecoder.class},
 		encoders= {GenericResponseEncoder.class})
 public class Endpoint {
-	private String sessionId;
+	private String sessionId = null;
 	
 	@OnOpen
 	public void onOpen(Session session) throws IOException {
@@ -55,30 +57,56 @@ public class Endpoint {
 			
 			break;
 		case CONNECT_TO_SESSION:
-			Request<SessionId> req_sessionId = (Request<SessionId>)req;
-			this.sessionId = req_sessionId.getContents().getSessionId();
+			Map<String, Object> contents = req.getContents();
+			if (!contents.containsKey("sessionId"))
+				throw new IllegalArgumentException("session id has not been specified");
+			
+			Object oSessionId = contents.get("sessionId");
+			if (oSessionId.getClass() != String.class)
+				throw new IllegalArgumentException("session id is not a string");
+			
+			this.sessionId = (String)oSessionId;
+			session.getBasicRemote().sendObject(Responses.success());
 			break;
 		case DISCONNECT_FROM_SESSION:
+			if (sessionId == null)
+				throw new RuntimeException("Client has not connected to a session");
+			
 			session.getBasicRemote().sendObject(Responses.success());
 			session.close();
 			break;
 		case CLOSE_SESSION:
+			if (sessionId == null)
+				throw new RuntimeException("Client has not connected to a session");
+			
 			session.getBasicRemote().sendObject(Responses.success());
 			session.close();
 			break;
 		case ADD_IMAGES:
+			if (sessionId == null)
+				throw new RuntimeException("Client has not connected to a session");
+			
 			session.getBasicRemote().sendObject(Responses.success());
 			break;
 		case DOWNLOAD_CONTENTS:
+			if (sessionId == null)
+				throw new RuntimeException("Client has not connected to a session");
+			
 			break;
 		case GET_DIRECTORY_INFO:
+			if (sessionId == null)
+				throw new RuntimeException("Client has not connected to a session");
+			
 			break;
 		case SYNCHRONIZE:
+			if (sessionId == null)
+				throw new RuntimeException("Client has not connected to a session");
+			
 			break;
 		default:
 			break;
 		}
-    }
+	}
 
     @OnClose
     public void onClose(Session session) throws IOException {
@@ -86,7 +114,17 @@ public class Endpoint {
     }
 
     @OnError
-    public void onError(Session session, Throwable throwable) throws IOException {
-    	session.getBasicRemote().sendObject(Responses.exception(throwable));
+    public void onError(Session session, Throwable throwable) throws IOException, EncodeException {
+    	if (throwable.getClass() == JsonbException.class) {
+    		String msg = throwable.getMessage();
+    		if (msg.contains("No enum constant")) {
+    			String className = RequestType.class.getCanonicalName().replace(".", "\\.");
+    			String absentConstant = msg.replaceAll(
+    					"^.*: No enum constant " + className + "\\.(.*?)$", "$1");
+    			
+    			session.getBasicRemote().sendObject(Responses.invalidMethod(absentConstant));
+    		}
+    	}
+    	else session.getBasicRemote().sendObject(Responses.exception(throwable));
     }
 }
