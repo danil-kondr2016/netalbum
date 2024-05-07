37bad3a55e407179a2eef087f654b5a8a8b0dc65
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-05-07 10:13:20 +0600

    netalbum-server: Endpoint: обработка растащена по отдельным функциям

--- netalbum-server/src/main/java/ru/danilakondr/netalbum/server/Endpoint.java
+++ netalbum-server/src/main/java/ru/danilakondr/netalbum/server/Endpoint.java
@@ -1,6 +1,7 @@
 package ru.danilakondr.netalbum.server;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Map;
 
 import javax.websocket.EncodeException;
@@ -8,12 +9,10 @@ import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
-import javax.websocket.RemoteEndpoint;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 import jakarta.json.bind.JsonbException;
-import ru.danilakondr.netalbum.api.SessionId;
 import ru.danilakondr.netalbum.api.request.Request;
 import ru.danilakondr.netalbum.api.request.RequestType;
 import ru.danilakondr.netalbum.api.response.Response;
@@ -47,66 +46,102 @@ public class Endpoint {
 	private void handleRequest(Session session, Request<Map<String, Object>> req) throws IOException, EncodeException {
 		switch (req.getMethod()) {
 		case INIT_SESSION:
-			if (sessionId != null)
-				throw new RuntimeException("Session already initiated");
-			
-			String id = SessionIdProvider.generateSessionId();
-			Response<?> resp = Responses.sessionId(id);
-			session.getBasicRemote().sendObject(resp);
-			this.sessionId = id;
-			
+			handleInitSession(session);
 			break;
 		case CONNECT_TO_SESSION:
-			Map<String, Object> contents = req.getContents();
-			if (!contents.containsKey("sessionId"))
-				throw new IllegalArgumentException("session id has not been specified");
-			
-			Object oSessionId = contents.get("sessionId");
-			if (oSessionId.getClass() != String.class)
-				throw new IllegalArgumentException("session id is not a string");
-			
-			this.sessionId = (String)oSessionId;
-			session.getBasicRemote().sendObject(Responses.success());
+			handleConnectToSession(session, req);
 			break;
 		case DISCONNECT_FROM_SESSION:
-			if (sessionId == null)
-				throw new RuntimeException("Client has not connected to a session");
-			
-			session.getBasicRemote().sendObject(Responses.success());
-			session.close();
+			handleDisconnectFromSession(session);
 			break;
 		case CLOSE_SESSION:
-			if (sessionId == null)
-				throw new RuntimeException("Client has not connected to a session");
-			
-			session.getBasicRemote().sendObject(Responses.success());
-			session.close();
+			handleCloseSession(session);
 			break;
 		case ADD_IMAGES:
-			if (sessionId == null)
-				throw new RuntimeException("Client has not connected to a session");
-			
-			session.getBasicRemote().sendObject(Responses.success());
+			handleAddImages(session);
 			break;
 		case DOWNLOAD_CONTENTS:
-			if (sessionId == null)
-				throw new RuntimeException("Client has not connected to a session");
-			
+			handleDownloadContents(session);
 			break;
 		case GET_DIRECTORY_INFO:
-			if (sessionId == null)
-				throw new RuntimeException("Client has not connected to a session");
-			
+			handleGetDirectoryInfo(session);
 			break;
 		case SYNCHRONIZE:
-			if (sessionId == null)
-				throw new RuntimeException("Client has not connected to a session");
-			
+			handleSynchronize(session, req);
 			break;
 		default:
+			session.getBasicRemote().sendObject(Responses.invalidMethod(req.getMethod().name() + " (not implemented)"));
 			break;
 		}
 	}
+
+	private void handleSynchronize(Session session, Request<Map<String, Object>> req) throws EncodeException, IOException {
+		if (sessionId == null)
+			throw new RuntimeException("Client has not connected to a session");
+
+		session.getBasicRemote().sendObject(Responses.invalidRequest("synchronization has not been implemented"));
+	}
+
+	private void handleGetDirectoryInfo(Session session) throws EncodeException, IOException {
+		if (sessionId == null)
+			throw new RuntimeException("Client has not connected to a session");
+
+		session.getBasicRemote().sendObject(Responses.directoryInfo("", 0));
+	}
+
+	private void handleDownloadContents(Session session) throws EncodeException, IOException {
+		if (sessionId == null)
+			throw new RuntimeException("Client has not connected to a session");
+
+		session.getBasicRemote().sendObject(Responses.directoryContents(new ArrayList<>()));
+	}
+
+	private void handleAddImages(Session session) throws IOException, EncodeException {
+		if (sessionId == null)
+			throw new RuntimeException("Client has not connected to a session");
+
+		session.getBasicRemote().sendObject(Responses.success());
+	}
+
+	private void handleCloseSession(Session session) throws IOException, EncodeException {
+		if (sessionId == null)
+			throw new RuntimeException("Client has not connected to a session");
+
+		session.getBasicRemote().sendObject(Responses.success());
+		session.close();
+	}
+
+	private void handleDisconnectFromSession(Session session) throws IOException, EncodeException {
+		if (sessionId == null)
+			throw new RuntimeException("Client has not connected to a session");
+
+		session.getBasicRemote().sendObject(Responses.success());
+		session.close();
+	}
+
+	private void handleInitSession(Session session) throws EncodeException, IOException {
+		if (sessionId != null)
+			throw new RuntimeException("Session already initiated");
+
+		String id = SessionIdProvider.generateSessionId();
+		Response<?> resp = Responses.sessionId(id);
+		session.getBasicRemote().sendObject(resp);
+		this.sessionId = id;
+	}
+
+	private void handleConnectToSession(Session session, Request<Map<String,Object>> req) throws EncodeException, IOException {
+		Map<String, Object> contents = req.getContents();
+		if (!contents.containsKey("sessionId"))
+			throw new IllegalArgumentException("session id has not been specified");
+
+		Object oSessionId = contents.get("sessionId");
+		if (oSessionId.getClass() != String.class)
+			throw new IllegalArgumentException("session id is not a string");
+
+		this.sessionId = (String)oSessionId;
+		session.getBasicRemote().sendObject(Responses.success());
+	}
+
 
     @OnClose
     public void onClose(Session session) throws IOException {
