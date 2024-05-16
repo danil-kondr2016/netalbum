fe2207814e944b72059d80d6b26e2cc6ac3d0b1f
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-05-16 16:00:45 +0600

    netalbum-api: Change: javadoc

--- netalbum-api/src/main/java/ru/danilakondr/netalbum/api/data/Change.java
+++ netalbum-api/src/main/java/ru/danilakondr/netalbum/api/data/Change.java
@@ -2,6 +2,15 @@ package ru.danilakondr.netalbum.api.data;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+/**
+ * Класс-держатель параметра изменения. Содержит два поля:
+ * <ul>
+ *     <li>{@code oldName}: старое имя</li>
+ *     <li>{@code newName}: новое имя (значение null означает, что файл удалён)</li>
+ * </ul>
+ *
+ * @author Данила А. Кондратенко
+ */
 @JsonPropertyOrder({"oldName", "newName"})
 public class Change {
 	private String oldName;
