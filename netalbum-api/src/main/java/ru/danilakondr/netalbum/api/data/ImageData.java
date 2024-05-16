ba8246432a67f3445621ff147aa79275f62ef6a6
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-05-16 16:00:34 +0600

    netalbum-api: ImageData: javadoc

--- netalbum-api/src/main/java/ru/danilakondr/netalbum/api/data/ImageData.java
+++ netalbum-api/src/main/java/ru/danilakondr/netalbum/api/data/ImageData.java
@@ -1,5 +1,17 @@
 package ru.danilakondr.netalbum.api.data;
 
+/**
+ * Класс-держатель данных изображения. Содержит поля:
+ * <ul>
+ *     <li>{@code fileName}: имя файла</li>
+ *     <li>{@code fileSize}: размер файла</li>
+ *     <li>{@code width}: ширина</li>
+ *     <li>{@code height}: высота</li>
+ *     <li>{@code thumbnail}: уменьшенная картинка</li>
+ * </ul>
+ *
+ * @author Данила А. Кондратенко
+ */
 public class ImageData extends ImageInfo {
 	private byte[] thumbnail;
 	
