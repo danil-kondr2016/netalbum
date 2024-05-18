50f294dc4bafb98cca620bd0ba33789442569659
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-05-18 11:01:09 +0600

    netalbum-client: NetAlbumService: очередь выполнения запросов

--- netalbum-client/src/main/java/ru/danilakondr/netalbum/client/connect/NetAlbumService.java
+++ netalbum-client/src/main/java/ru/danilakondr/netalbum/client/connect/NetAlbumService.java
@@ -9,18 +9,20 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.WebSocket;
 import java.util.concurrent.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class NetAlbumService extends SubmissionPublisher<Response>
         implements WebSocket.Listener {
     private final BlockingQueue<Response> responses = new SynchronousQueue<>();
-    private final CountDownLatch latch = new CountDownLatch(1);
+    private final CountDownLatch connectionLatch = new CountDownLatch(1);
     private final ExecutorService service;
     private volatile boolean connected = false;
     private WebSocket socket;
 
     public NetAlbumService() {
         super(Executors.newSingleThreadExecutor(), 1000);
-        this.service = Executors.newFixedThreadPool(50);
+        this.service = Executors.newSingleThreadExecutor();
     }
 
     public void connectTo(URI uri) {
@@ -33,24 +35,36 @@ public class NetAlbumService extends SubmissionPublisher<Response>
                     .newWebSocketBuilder()
                     .buildAsync(uri, NetAlbumService.this);
             cfWebSocket.join();
-            latch.countDown();
+            connectionLatch.countDown();
         });
     }
 
     public void waitUntilConnected() throws InterruptedException {
         System.out.println("Start waiting");
-        latch.await();
+        connectionLatch.await();
         System.out.println("Stop waiting");
     }
 
     public void putRequest(Request req) {
         service.execute(() -> {
-            ObjectMapper mapper = new ObjectMapper();
             try {
+                ObjectMapper mapper = new ObjectMapper();
                 String msg = mapper.writeValueAsString(req);
-                socket.sendText(msg, true).join();
+                socket.sendText(msg, true).get();
             } catch (JsonProcessingException e) {
                 throw new RuntimeException(e);
+            } catch (InterruptedException ex) {
+                Response.Error err = new Response.Error(Response.Error.Status.EXCEPTION);
+                err.setProperty("message", ex);
+                submit(err);
+
+                Logger.getLogger(NetAlbumService.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ExecutionException ex) {
+                Response.Error err = new Response.Error(Response.Error.Status.EXCEPTION);
+                err.setProperty("message", ex);
+                submit(err);
+
+                Logger.getLogger(NetAlbumService.class.getName()).log(Level.SEVERE, null, ex);
             }
         });
     }
@@ -66,6 +80,7 @@ public class NetAlbumService extends SubmissionPublisher<Response>
     public void onOpen(WebSocket webSocket) {
         webSocket.request(1);
         this.socket = webSocket;
+        this.connected = true;
     }
 
     @Override
@@ -76,6 +91,7 @@ public class NetAlbumService extends SubmissionPublisher<Response>
         if (last) {
             processResponse(sb.toString());
             sb.setLength(0);
+            
             accumulatedMessage.complete(null);
             CompletionStage<?> cf = accumulatedMessage;
             accumulatedMessage = new CompletableFuture<>();
@@ -87,6 +103,7 @@ public class NetAlbumService extends SubmissionPublisher<Response>
 
     @Override
     public CompletionStage<?> onClose(WebSocket webSocket, int statusCode, String reason) {
+        System.out.printf("Closed %d %s%n", statusCode, reason);
         connected = false;
         return WebSocket.Listener.super.onClose(webSocket, statusCode, reason);
     }
