1f59d4b19cc5ab24dcea37c795833b77201a496c
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-05-19 17:09:21 +0600

    netalbum-client: SessionTable: AbstractTableModel, getSessionAt

--- netalbum-client/src/main/java/ru/danilakondr/netalbum/client/connect/SessionTable.java
+++ netalbum-client/src/main/java/ru/danilakondr/netalbum/client/connect/SessionTable.java
@@ -5,28 +5,22 @@
 package ru.danilakondr.netalbum.client.connect;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
-import java.util.Set;
-import javax.swing.event.TableModelEvent;
-import javax.swing.event.TableModelListener;
-import javax.swing.table.TableModel;
+import javax.swing.table.AbstractTableModel;
 
 /**
  *
  * @author danko
  */
-public class SessionTable implements TableModel {
+public class SessionTable extends AbstractTableModel {
     private final List<Session> sessionList;
-    private final Set<TableModelListener> listeners;
     private static final String[] COLUMN_NAMES = new String[]{
         "URL-адрес сервера", "Ключ сессии", "Путь к папке с фотографиями"
     };
     
     public SessionTable() {
+        super();
         this.sessionList = new ArrayList<>();
-        this.listeners = new HashSet<>();
     }
 
     @Override
@@ -68,74 +62,18 @@ public class SessionTable implements TableModel {
         }
     }
 
-    @Override
-    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-        switch (columnIndex) {
-            case 0:
-                sessionList.get(rowIndex).setUrl(Objects.toString(aValue));
-                break;
-            case 1:
-                sessionList.get(rowIndex).setSessionId(Objects.toString(aValue));
-                break;
-            case 2:
-                sessionList.get(rowIndex).setPath(Objects.toString(aValue));
-                break;
-            default:
-                throw new ArrayIndexOutOfBoundsException();
-        }
-        
-        notifyAboutUpdate(rowIndex, columnIndex);
-    }
-    
-    private void notifyAboutUpdate(int rowIndex, int columnIndex) {
-        TableModelEvent evt = new TableModelEvent(this, rowIndex, columnIndex);
-
-        for (TableModelListener l: listeners) {
-            l.tableChanged(evt);
-        }
-    }
-    
-    private void notifyAboutDelete(int row) {
-        TableModelEvent evt = new TableModelEvent(this, 
-                row, row, 
-                TableModelEvent.ALL_COLUMNS, 
-                TableModelEvent.DELETE);
-
-        for (TableModelListener l: listeners) {
-            l.tableChanged(evt);
-        }
-    }
-    
-    private void notifyAboutAdd(int row) {
-        TableModelEvent evt = new TableModelEvent(this, 
-                row, row, 
-                TableModelEvent.ALL_COLUMNS, 
-                TableModelEvent.INSERT);
-
-        for (TableModelListener l: listeners) {
-            l.tableChanged(evt);
-        }
-    }
-
-    @Override
-    public void addTableModelListener(TableModelListener l) {
-        listeners.add(l);
-    }
-
-    @Override
-    public void removeTableModelListener(TableModelListener l) {
-        listeners.remove(l);
-    }
-    
     public void addSession(Session session) {
         sessionList.add(session);
-        session.addOnCloseListener(s -> SessionTable.this.removeSession(s));
-        notifyAboutAdd(sessionList.size() - 1);
+        fireTableRowsInserted(sessionList.size()-1, sessionList.size()-1);
     }
     
     public void removeSession(Session session) {
         int index = sessionList.indexOf(session);
         sessionList.remove(index);
-        notifyAboutDelete(index);
+        fireTableRowsDeleted(index, index);
+    }
+    
+    public Session getSessionAt(int index) {
+        return sessionList.get(index);
     }
 }
